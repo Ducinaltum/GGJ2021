f8ef29958f0043dc7acfef18c728170aad9f1594
Ducinaltum <alejandro_beacon@hotmail.com>
2021-01-30 22:09:02 -0300

    small changes

--- GGJ 2021/Assets/Scripts/CameraFollow.cs
+++ GGJ 2021/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,13 @@ public class CameraFollow : MonoBehaviour
     public Transform obj;
     public float offX;
     public float offY;
+
+    public float offsetY = 1f;
+    public float maxCameraSize; 
+    public float minCameraSize; 
+    public float mouseScroll;
+
+    public Camera cam;
     void Start()
     {
         transform.position = new Vector3(obj.position.x, obj.position.y, transform.position.z);
@@ -15,10 +22,9 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(obj.position.x > offX || obj.position.y > offY) {
-            Vector3 target = new Vector3(obj.position.x, obj.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
-        //}
+        Vector3 target = new Vector3(obj.position.x,  obj.position.y+ offsetY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + (-Input.mouseScrollDelta.y * mouseScroll * Time.deltaTime), minCameraSize, maxCameraSize);
     }
 
 
